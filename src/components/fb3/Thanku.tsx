0722b603937c3355012c1c45162b76880b9d44a8
offbeatculture <tools.offbeatculture@gmail.com>
2026-06-29 17:59:58 +0530

    update WA link

--- src/components/fb3/Thanku.tsx
+++ src/components/fb3/Thanku.tsx
@@ -21,7 +21,7 @@ export const ThankYouPage = () => {
         {/* WhatsApp Group CTA */}
         <a
           // href="https://energyshift.valarmathisrinivasan.in/erc-wap-fb"
-          href="http://join.askvalarrmathi.com/bcr-wap"
+          href="http://join.valarmathisrinivasan.in/fb-wap"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center gap-2 rounded-full bg-[#175B53] hover:bg-[#134741] px-6 py-3 text-white font-semibold shadow-md transition-all duration-300"
